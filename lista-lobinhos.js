3fa0c3de935d6b91938a081312dd525ad1fea5c5
Daniel Ongarato de Arruda <dongarato.arruda@gmail.com>
2026-02-07 11:07:53 -0300

    Página QuemSomos concluída e Página ListaDeLobinhos avançada

---
+++ lista-lobinhos.js
@@ -0,0 +1,84 @@
+const API_URL = "http://localhost:3000/lobinhos";
+const listCard = document.querySelector("#listCard");
+
+let paginaAtual = 1;
+let termoBusca = "";
+let filtrarAdotados = false;
+
+const busca = document.querySelector("#barraDePesquisa");
+busca.addEventListener("input", (event) => {
+  termoBusca = busca.value;
+  paginaAtual = 1;
+  inicializarPagina();
+});
+
+const filtroAdotados = document.querySelector("#verAdotados");
+filtroAdotados.addEventListener("change", (event) => {
+  filtrarAdotados = filtroAdotados.checked;
+  paginaAtual = 1;
+  inicializarPagina();
+});
+
+function montarURL() {
+  let url = `${API_URL}?_page=${paginaAtual}&_limit=4`;
+
+  if (termoBusca !== "") {
+    url += `&nome_like=${termoBusca}`;
+  }
+
+  if (filtrarAdotados) {
+    url += `&adotado=true`;
+  }
+
+  return url;
+}
+
+async function buscarLobinhos() {
+    const url = montarURL();
+
+    //fetch na api
+    const response = await fetch(url);
+
+    if (!response.ok) {
+        throw new Error(`Erro HTTP: ${resposta.status}`);
+    }
+
+    //json dos lobinhos
+    const lobinhos = await response.json();
+    return lobinhos;
+}
+
+function criarCardLobinho(lobinhos) {
+    listCard.innerHTML = '';
+    for (const lobo of lobinhos) {
+        const li = document.createElement("li");
+        li.className = "listCard";
+
+        li.innerHTML = `
+            <article class="cardLobinho">
+                <img src="${lobo.imagem}" alt="Foto do lobinho ${lobo.nome}">
+                <div class="dadosLobinho">
+                    <h3>${lobo.nome}</h3>
+                    <p>Idade: ${lobo.idade} anos</p>
+                    <p>${lobo.descricao}</p>
+                </div>
+            </article>
+        `;
+
+        listCard.appendChild(li);
+    }
+}
+
+async function inicializarPagina() {
+    try {
+        const lobinhos = await buscarLobinhos();
+        criarCardLobinho(lobinhos);
+        //spanPagina.textContent = `Página ${paginaAtual}`;
+    }
+
+    catch (error) {
+        console.error("Erro ao carregar os lobinhos:", error);
+    }
+}
+
+inicializarPagina();
