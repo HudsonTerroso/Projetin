0b630db768443c166e0ace578ce6fd90899f7041
Isabella Direito <isa.martinsj3@gmail.com>
2026-02-08 16:57:20 -0300

    paginacao pronta

--- lista-lobinhos.js
+++ lista-lobinhos.js
@@ -5,6 +5,10 @@ let paginaAtual = 1;
 let termoBusca = "";
 let filtrarAdotados = false;
 
+const limite = 4; 
+let totalpag = 1;
+const pag = document.querySelector(".paginacao");
+
 const busca = document.querySelector("#barraDePesquisa");
 busca.addEventListener("input", (event) => {
   termoBusca = busca.value;
@@ -42,11 +46,59 @@ async function buscarLobinhos() {
     if (!response.ok) {
         throw new Error(`Erro HTTP: ${response.status}`);
     }
-
+    const totalItens = response.headers.get('X-Total-Count');
+    totalpag = Math.ceil(parseInt(totalItens) / limite);
+  
     //json dos lobinhos
     const lobinhos = await response.json();
     return lobinhos;
 }
+
+function paginacao(){
+
+    pag.innerHTML = "";
+
+    let inicio = paginaAtual - 1;
+    let final = paginaAtual + 3;
+
+    if(inicio < 1) inicio = 1;
+    if(final > totalpag) final = totalpag;
+
+    if(inicio >= 2){
+        const ini = document.createElement("span");
+        ini.textContent = `<<<...`;
+        ini.classList.add("span");
+        ini.addEventListener("click", ()=>{
+            paginaAtual = paginaAtual -2;
+            inicializarPagina();
+        });
+        pag.append(ini);
+    }
+
+    for(let i= inicio; i <= final; i++){
+        const bloco = document.createElement("span");
+        bloco.textContent = ` ${i} `;
+        bloco.classList.add("span");
+
+        bloco.addEventListener("click", ()=>{
+            paginaAtual = i;
+            inicializarPagina();
+        });
+
+        pag.append(bloco);
+    }
+
+    const fim = document.createElement("span");
+    fim.textContent = `... >>>`;
+    fim.classList.add("span");
+    fim.addEventListener("click", ()=>{
+        paginaAtual = paginaAtual + 2;
+        inicializarPagina();
+    });
+
+    pag.append(fim);
+}
+
 
 function criarCardLobinho(lobinhos) {
     listCard.innerHTML = '';
@@ -54,23 +106,7 @@ function criarCardLobinho(lobinhos) {
         const li = document.createElement("li");
         li.className = "listCard";
 
-        if (lobo.adotado){
-            li.innerHTML = `
-            <article class="cardLobinho">
-                <img class="imagemLobinho" src="${lobo.imagem}" alt="Foto do lobinho ${lobo.nome}">
-                <div class="dadosLobinho">
-                    <h3 class="h3">${lobo.nome}</h3>
-                    <a class="botaoAdotar adotado" href="#">Adotado</a>
-                    <p class="idade">Idade: ${lobo.idade} anos</p>
-                    <p class="descricaoLista">${lobo.descricao}</p>
-                    
-                </div>
-            </article>
-        `;
-
-        listCard.appendChild(li)
-        }else{
-            li.innerHTML = `
+        li.innerHTML = `
             <article class="cardLobinho">
                 <img class="imagemLobinho" src="${lobo.imagem}" alt="Foto do lobinho ${lobo.nome}">
                 <div class="dadosLobinho">
@@ -83,16 +119,15 @@ function criarCardLobinho(lobinhos) {
             </article>
         `;
 
-        listCard.appendChild(li)
-        }
+        listCard.appendChild(li);
     }
 }
 
 async function inicializarPagina() {
     try {
         const lobinhos = await buscarLobinhos();
+        paginacao();
         criarCardLobinho(lobinhos);
-        //spanPagina.textContent = `Página ${paginaAtual}`;
     }
 
     catch (error) {
@@ -101,4 +136,5 @@ async function inicializarPagina() {
 }
 
 inicializarPagina();
+
 
